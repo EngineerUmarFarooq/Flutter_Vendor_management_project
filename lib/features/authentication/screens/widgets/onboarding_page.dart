34a36ca418c5560a4291fda66ecb1121aacbab9b
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-04-13 00:21:16 +0500

    Update onboarding_page.dart

--- lib/features/authentication/screens/widgets/onboarding_page.dart
+++ lib/features/authentication/screens/widgets/onboarding_page.dart
@@ -12,26 +12,7 @@ class OnBoardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(VSizes.defaultSpace),
-        child: Column(
-          children: [
-            Image(
-              width: VHelperFunctions.screenWidth() * 0.8,
-              height: VHelperFunctions.screenHeight() * 0.5,
-              image: AssetImage(image),
-            ), // Image
-            Text(
-              title,
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headlineMedium,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: VSizes.spaceBtwItems,
-            ),
-            Text(
+  
               subtitle,
               style: Theme
                   .of(context)
