fef949ca0d6b97259b80f44753066155fceba3a7
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-04-13 00:18:48 +0500

    Update form_divider.dart

--- lib/common/widgets.login_signup/form_divider.dart
+++ lib/common/widgets.login_signup/form_divider.dart
@@ -7,17 +7,7 @@ import '../../utils/constants/text_strings.dart';
 class VFormDivider extends StatelessWidget {
   const VFormDivider({ super.key, required this.dividerText});
 
-final String dividerText;
 
-  @override
-  Widget build(BuildContext context) {
-    final dark = VHelperFunctions.isDarkMode(context);
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(child: Divider(color: dark? VColors.darkGrey: VColors.grey, thickness: 0.5, indent: 60, endIndent:5)),
-          Text(dividerText,style: Theme.of(context).textTheme.labelMedium,),
-          Flexible(child: Divider(color: dark? VColors.darkGrey: VColors.grey, thickness: 0.5, indent: 5, endIndent:60)),
         ]  );
   }
 }
