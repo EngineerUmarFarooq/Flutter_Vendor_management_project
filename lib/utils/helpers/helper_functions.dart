0b4c00225d9e76db55af6926ba8eae49e458a8b6
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-04-13 00:22:19 +0500

    Update helper_functions.dart

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -3,50 +3,7 @@ import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 class VHelperFunctions {
   static Color? getColor(String value) {
-    /// Define your product specific colors here and it will match the attribute colors and show specific
-    if (value == 'Green') {
-      return Colors.green;
-    } else if (value == 'Green') {
-      return Colors.green;
-    } else if (value == 'Red') {
-      return Colors.red;
-    } else if (value == 'Blue') {
-      return Colors.blue;
-    } else if (value == 'Pink') {
-    return Colors.pink;
-    } else if (value == 'Grey') {
-  return Colors.grey;
-  } else if (value == 'Purple') {
-  return Colors.purple;
-  } else if (value == 'Black') {
-  return Colors.black;
-  } else if (value == 'White') {
-  return Colors.white;
-  } else if (value == 'Brown') {
-  return Colors.brown;
-  } else if (value == 'Teal') {
-  return Colors.teal;
-  } else if (value == 'Indigo') {
-  return Colors.indigo;
-  } else {
-  return null;
-  }
-}
-  static void showSnackBar (String message) {
-    ScaffoldMessenger.of(Get.context!).showSnackBar (
-      SnackBar (content: Text (message)),
-    );
-  }
-  static void showAlert(String title, String message) {
-    showDialog(
-      context: Get.context!,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(message),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
+  vigator.of(context).pop(),
               child: const Text('OK'),
             ), // TextButton
           ],
@@ -75,21 +32,7 @@ class VHelperFunctions {
   static double screenHeight() {
     return MediaQuery.of(Get.context!).size.height;
   }
-  static double screenWidth() {
-    return MediaQuery.of(Get.context!).size.width;
-  }
-  static String getFormattedDate (DateTime date, {String format = 'dd MMM yyyy'}) {
-  return DateFormat(format).format(date);
-  }
-  static List<T> removeDuplicates<T>(List<T> list) {
-  return list.toSet().toList();
-  }
-  static List<Widget> wrapWidgets (List<Widget> widgets, int rowSize) {
-  final wrappedList = <Widget>[];
-  for (var i = 0; i < widgets.length; i += rowSize) {
-  final rowChildren =widgets.sublist(i, i + rowSize > widgets.length? widgets.length: 1 + rowSize);
-  wrappedList.add(Row(children: rowChildren));
-  }
+\ }
   return wrappedList;
   }
 }
