dbe5cc350dcc2e323d8af475d47c0c043eba863f
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-04-13 00:22:41 +0500

    Update helper_functions.dart

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -19,18 +19,7 @@ class VHelperFunctions {
   static String truncateText(String text, int maxLength) {
     if (text.length <= maxLength) {
       return text;
-    } else {
-      return '${text.substring(0, maxLength)}...';
-  }
-  }
-  static bool isDarkMode (BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark;
-  }
-  static Size screenSize() {
-    return MediaQuery.of(Get.context!).size;
-}
-  static double screenHeight() {
-    return MediaQuery.of(Get.context!).size.height;
+  
   }
 \ }
   return wrappedList;
