d5baf5f17ad8f9771260b0eca261f972a6154162
EngineerUmarFarooq <149157063+EngineerUmarFarooq@users.noreply.github.com>
2024-04-13 00:20:19 +0500

    Update onboarding.dart

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -23,31 +23,7 @@ class OnBoardingScreen extends StatelessWidget {
     final controller = Get.put(OnBoardingController());
     return Scaffold(
         body: Stack(children: [
-      /// Horizontal Scrollable Pages
-      PageView(
-        controller: controller.pageController,
-        onPageChanged: controller.updatePageIndicator,
-        children: const [
-          OnBoardingPage(
-            image: VImages.onBoardingImage1,
-            title: VTexts.onBoardingTitle1,
-            subtitle: VTexts.onBoardingSubTitle1,
-          ),
-          OnBoardingPage(
-            image: VImages.onBoardingImage2,
-            title: VTexts.onBoardingTitle2,
-            subtitle: VTexts.onBoardingSubTitle2,
-          ),
-          OnBoardingPage(
-            image: VImages.onBoardingImage3,
-            title: VTexts.onBoardingTitle3,
-            subtitle: VTexts.onBoardingSubTitle3,
-          ),
-        ],
-      ), // Column
-      ///Skip button
-      const OnBoardingSkip(), // Positioned
-      // TextButton
+     
       ///Dot navigation smoothPageIndicator
       const OnBoardingDotNavigation(),// Positioned
           /// Circular Button
